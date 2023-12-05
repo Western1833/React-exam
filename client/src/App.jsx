2c0a962b6842db59d34ac077d43edbd8fe5ae1e6
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-12-05 23:47:47 +0200

    Update app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import Registration from './components/Authorization/register/Registration.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import Login from './components/Authorization/login/Login.jsx';
+import Cars from './components/Cars/Cars.jsx';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/cars' element={<Cars />} />
         <Route path='/register' element={<Registration />} />
         <Route path='/login' element={<Login />} />
       </Routes>

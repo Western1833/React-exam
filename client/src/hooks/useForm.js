559d41d04b81128aa8c682f42988b342920af03f
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-12-09 20:07:49 +0200

    Create custom hook 'useForm'

---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function useForm(submitHandler, initialValues){
+    const [values, setValues] = useState(initialValues);
+
+    const onChange = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    const onSubmit = async(e) => {
+        e.preventDefault();
+
+        submitHandler(values);
+    }
+
+    return {
+        values,
+        onChange
+    }
+}

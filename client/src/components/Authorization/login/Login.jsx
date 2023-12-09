67f8c527267b3eb67f18288ce855ad71aba4013b
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-12-09 22:57:24 +0200

    Update/fix for Login.jsx

--- client/src/components/Authorization/login/Login.jsx
+++ client/src/components/Authorization/login/Login.jsx
@@ -1,15 +1,13 @@
 import LoginForm from "./LoginForm.jsx";
 import styles from './login.module.css';
 
-export default function Login({
-    loginSubmitHandler
-}) {
+export default function Login() {
     return (
         <section className={styles['loginContainer']}>
             <div className={styles['imageContainer']}>
                 <img src="/images/login-image.jpg" alt="" className={styles['image']} />
                 <div className={styles['login']}>
-                    <LoginForm loginSubmitHandler={loginSubmitHandler}/>
+                    <LoginForm />
                 </div>
             </div>
         </section>
